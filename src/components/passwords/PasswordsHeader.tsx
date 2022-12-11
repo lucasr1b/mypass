d82f454c1bdfcdee06ff4b9b3b377d022caefcb2
LucasCodes01 <lucascodesweb@gmail.com>
2022-12-12 12:29:00 +1300

    Styled search input

--- src/components/passwords/PasswordsHeader.tsx
+++ src/components/passwords/PasswordsHeader.tsx
@@ -1,14 +1,21 @@
-import './PasswordsHeader.scss';
+import { Search } from "react-bootstrap-icons";
+import "./PasswordsHeader.scss";
 
 const PasswordsHeader = () => {
-  return (
-    <div className='Passwords__Header'>
-      <span>22 passwords</span>
-      <div className='Password__Search'>
-        <input />
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className='Passwords__Header'>
+			<span>22 passwords</span>
+			<div className='Password__Search'>
+				<input
+					placeholder='Search passwords'
+					className='Password__Search__Input'
+				/>
+				<div className='Password__Search__Icon'>
+					<Search />
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default PasswordsHeader;
