e08fb7a0c0107c0bba69b24e91e162645a19cbb6
LucasCodes01 <lucascodesweb@gmail.com>
2022-12-12 15:31:45 +1300

    Renamed navbar dark mode to theme and centered icon

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = (props: any) => {
     <nav className='Navbar'>
       <img src='/logo.svg' className='Navbar__Logo' alt={'mypass'} />
       <div className='Navbar__Buttons'>
-        <button className='Navbar__Dark__Mode' onClick={props.switchTheme}>{props.theme === 'light' ? <MoonFill /> : <SunFill />}</button>
+        <button className='Navbar__Theme' onClick={props.switchTheme}>{props.theme === 'light' ? <MoonFill /> : <SunFill />}</button>
         <button className='Navbar__Profile'><img src='profile.jpg' alt={'profile'} /> <span>Lucas</span> <CaretDownFill /></button>
       </div>
     </nav>
