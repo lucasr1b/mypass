4fae998211942274581fe2d1e5c8386449000467
LucasCodes01 <lucascodesweb@gmail.com>
2022-12-12 14:56:03 +1300

    Added dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import Navbar from './components/navbar/Navbar';
 import WelcomeBanner from './components/welcome/WelcomeBanner';
 import Passwords from './components/passwords/Passwords';
+import useLocalStorage from 'use-local-storage';
 import './App.scss';
 
 const App = () => {
+
+  const [theme, setTheme] = useLocalStorage('theme', 'light');
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  }
+
   return (
-    <div className='App'>
-      <Navbar />
+    <div className='App' data-theme={theme}>
+      <Navbar switchTheme={() => switchTheme()} />
       <div className='App__Container'>
         <WelcomeBanner />
         <Passwords />
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,12 @@
 import './Navbar.scss';
 import { MoonFill, CaretDownFill } from 'react-bootstrap-icons';
 
-const Navbar = () => {
+const Navbar = ({ switchTheme }: any) => {
   return (
     <nav className='Navbar'>
       <img src='/logo.svg' className='Navbar__Logo' />
       <div className='Navbar__Buttons'>
-        <button className='Navbar__Dark__Mode'><MoonFill /></button>
+        <button className='Navbar__Dark__Mode' onClick={switchTheme}><MoonFill /></button>
         <button className='Navbar__Profile'><img src='profile.jpg' /> <span>Lucas</span> <CaretDownFill /></button>
       </div>
     </nav>
