c0cbc5e5b637d58f0fa98effbc33407fc7b86e7e
LucasCodes01 <lucascodesweb@gmail.com>
2022-12-13 22:01:05 +1300

    Added TypeScript, concurrently and nodemon to server

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,10 @@
-const express = require('express');
+import express, { Express, Request, Response } from 'express';
 require('dotenv').config();
 
-const app = express();
+const app: Express = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
