da82e4e78985ae542449ffa83b1efd2cd4896583
LucasCodes01 <lucascodesweb@gmail.com>
2022-12-15 10:55:40 +1300

    Added form footer and fixed typo + adjusted form width

--- client/src/Register.tsx
+++ client/src/Register.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import FormFooter from './components/form/FormFooter';
 import FormHeader from './components/form/FormHeader';
 import FormInput from './components/form/FormInput';
 import FormSubmitButton from './components/form/FormSubmitButton';
@@ -12,13 +13,14 @@ const Register = () => {
       <Navbar buttonsEnabled={false} />
       <div className='Register__Container'>
         <div className='Register__Form'>
-          <FormHeader title='Create an account' description='Create an account and start ssaving your passwords.' />
+          <FormHeader title='Create an account' description='Create an account and start saving your passwords.' />
           <FormInput label={'Full name'} />
           <FormInput label={'Email address'} />
           <FormInput label={'Password'} />
           <FormInput label={'Confirm password'} />
           <FormSubmitButton text='Create account' />
           <FormWithGoogle text='Sign up with Google' />
+          <FormFooter text='Already have an account?' action='Login' link='/login' />
         </div>
         <div className='Register__Image'>
           <img src='register.svg' />
---
+++ client/src/components/form/FormFooter.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import './FormFooter.scss';
+
+export type FormFooterProps = {
+  text: string;
+  action: string;
+  link: string;
+}
+
+const FormFooter = (props: FormFooterProps) => {
+  return (
+    <div className='Form__Footer'>
+      {props.text} <Link to={props.link}>{props.action}</Link>
+    </div>
+  )
+}
+
+export default FormFooter;
