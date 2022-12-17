7232d79634cf772779fe89e07f86a260c68b2c9a
LucasCodes01 <lucascodesweb@gmail.com>
2022-12-18 10:59:13 +1300

    Added dropdown to profile

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,17 +1,32 @@
+import { useState } from 'react';
 import './Navbar.scss';
-import { MoonFill, CaretDownFill, SunFill } from 'react-bootstrap-icons';
+import { MoonFill, CaretDownFill, CaretUpFill, SunFill, GearFill, DoorOpenFill, MoonStarsFill } from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom';
 
 const Navbar = (props: any) => {
+
+  const [dropdownToggled, setDropdownToggled] = useState(false);
+
   return (
     <nav className='Navbar'>
       <img src='/logo.svg' className='Navbar__Logo' alt={'mypass'} />
       {props.buttonsEnabled &&
-        <div className='Navbar__Buttons'>
-          <button className='Navbar__Theme' onClick={props.switchTheme}>{props.theme === 'light' ? <MoonFill /> : <SunFill />}</button>
-          <button className='Navbar__Profile'><img src='profile.jpg' alt={'profile'} /> <span>Lucas</span> <CaretDownFill /></button>
-        </div>
+        <>
+          <div className='Navbar__Buttons'>
+            <button className='Navbar__Theme' onClick={props.switchTheme}>{props.theme === 'light' ? <MoonFill /> : <SunFill />}</button>
+            <div className='Navbar__Profile'>
+              <button onClick={() => setDropdownToggled(!dropdownToggled)}>
+                <img src='profile.jpg' alt={'profile'} /> <span>Lucas</span> {dropdownToggled ? <CaretUpFill /> : <CaretDownFill />}
+              </button>
+              {dropdownToggled && <div className='Navbar__Dropdown'>
+                <a><GearFill /> Settings</a>
+                <a><MoonStarsFill />Theme</a>
+                <Link to='/logout'><DoorOpenFill />Logout</Link>
+              </div>}
+            </div>
+          </div>
+        </>
       }
-
     </nav>
   )
 }
