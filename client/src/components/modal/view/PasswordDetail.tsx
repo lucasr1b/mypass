f971817cde517a4ea5c8bbd28cebda9e58e60993
LucasCodes01 <lucascodesweb@gmail.com>
2022-12-18 23:32:50 +1300

    Added copy to clipboard message and functionality

--- client/src/components/modal/view/PasswordDetail.tsx
+++ client/src/components/modal/view/PasswordDetail.tsx
@@ -12,19 +12,28 @@ const PasswordDetail = (props: PasswordDetailProps) => {
 
   const [isHidden, setIsHidden] = useState(true);
   const [type, setType] = useState('password');
+  const [isCopied, setIsCopied] = useState(false);
 
-  const toggleHide = (e: any) => {
+  const toggleHide = () => {
     isHidden ? setType('text') : setType('password');
     setIsHidden(!isHidden);
   }
 
+  const copy = (detail: string) => {
+    setIsCopied(true);
+    navigator.clipboard.writeText(detail);
+  }
+
   return (
     <div className='Password__Detail'>
-      <input disabled type={props.type === 'password' ? type : props.type} value={props.value} />
-      <div className='Password__Detail__Actions'>
-        <div className={props.type === 'password' ? '' : 'Password__Detail__Action__Disabled'} onClick={toggleHide}><FontAwesomeIcon icon={isHidden ? faEye : faEyeSlash} /></div>
-        <div><FontAwesomeIcon icon={faCopy} /></div>
+      <div className='Password__Detail__Input'>
+        <input disabled type={props.type === 'password' ? type : props.type} value={props.value} />
+        <div className='Password__Detail__Actions'>
+          <div className={props.type === 'password' ? '' : 'Password__Detail__Action__Disabled'} onClick={toggleHide}><FontAwesomeIcon icon={isHidden ? faEye : faEyeSlash} /></div>
+          <div onClick={() => copy(props.value)}><FontAwesomeIcon icon={faCopy} /></div>
+        </div>
       </div>
+      {isCopied && <span>Copied to clipboard</span>}
     </div>
   )
 
