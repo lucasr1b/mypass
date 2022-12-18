40d823172a68be75890d07c36a73fd3b61fc8043
LucasCodes01 <lucascodesweb@gmail.com>
2022-12-18 13:34:11 +1300

    Display fetched passwords

--- client/src/components/passwords/Password.tsx
+++ client/src/components/passwords/Password.tsx
@@ -3,19 +3,21 @@ import { ChevronRight } from 'react-bootstrap-icons';
 
 export type PasswordProps = {
 	password: {
-		icon: string;
-		site: string;
-		email: string;
+		identifier: string;
+		url: string;
+		details: string;
+		password: string;
+		logo: string;
 	}
 }
 
 const Password = (props: PasswordProps) => {
 	return (
 		<div className='Password'>
-			<img src='icons/Google.png' alt={props.password.site} />
+			<img src='icons/Google.png' alt={props.password.logo} />
 			<div className='Password__Details'>
-				<span className='Password__Details__Site'>{props.password.site}</span>
-				<span className='Password__Details__Email'>{props.password.email}</span>
+				<span className='Password__Details__Site'>{props.password.identifier}</span>
+				<span className='Password__Details__Email'>{props.password.details}</span>
 			</div>
 			<div className='Password__View'>
 				<ChevronRight />
--- client/src/components/passwords/PasswordList.tsx
+++ client/src/components/passwords/PasswordList.tsx
@@ -1,48 +1,82 @@
+import { useEffect, useState } from 'react';
 import Password from "./Password";
+import { axiosConfig } from '../../utils/constants';
+import axios from 'axios';
+
+export type Password = {
+  identifier: string;
+  url: string;
+  details: string;
+  password: string;
+  logo: string;
+}
 
 const PasswordList = () => {
 
-  const passwords = [
-    {
-      icon: 'Google.png',
-      site: 'Google',
-      email: 'lucas@lucasribeiro.co',
-    },
-    {
-      icon: 'Google.png',
-      site: 'Chess',
-      email: 'lucas@lucasribeiro.co',
-    },
-    {
-      icon: 'Google.png',
-      site: 'Netflix',
-      email: 'lucas@lucasribeiro.co',
-    },
-    {
-      icon: 'Google.png',
-      site: 'Spotify',
-      email: 'lucas@lucasribeiro.co',
-    },
-    {
-      icon: 'Google.png',
-      site: 'YouTube',
-      email: 'lucas@lucasribeiro.co',
-    },
-    {
-      icon: 'Google.png',
-      site: 'Instagram',
-      email: 'lucas@lucasribeiro.co',
-    },
-    {
-      icon: 'Google.png',
-      site: 'GitHub',
-      email: 'lucas@lucasribeiro.co',
-    },
-  ]
+  const [passwords, setPasswords] = useState<Password[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPasswords = async () => {
+      await axios.get('http://localhost:5000/api/passwords', axiosConfig)
+        .then((res) => {
+          setPasswords(res.data)
+          setIsLoading(false);
+        })
+        .catch((res) => {
+          console.log(res.response.data.error);
+        })
+    }
+    fetchPasswords();
+  }, [])
+
+  // const passwords = [
+  //   {
+  //     icon: 'Google.png',
+  //     site: 'Google',
+  //     email: 'lucas@lucasribeiro.co',
+  //   },
+  //   {
+  //     icon: 'Google.png',
+  //     site: 'Chess',
+  //     email: 'lucas@lucasribeiro.co',
+  //   },
+  //   {
+  //     icon: 'Google.png',
+  //     site: 'Netflix',
+  //     email: 'lucas@lucasribeiro.co',
+  //   },
+  //   {
+  //     icon: 'Google.png',
+  //     site: 'Spotify',
+  //     email: 'lucas@lucasribeiro.co',
+  //   },
+  //   {
+  //     icon: 'Google.png',
+  //     site: 'YouTube',
+  //     email: 'lucas@lucasribeiro.co',
+  //   },
+  //   {
+  //     icon: 'Google.png',
+  //     site: 'Instagram',
+  //     email: 'lucas@lucasribeiro.co',
+  //   },
+  //   {
+  //     icon: 'Google.png',
+  //     site: 'GitHub',
+  //     email: 'lucas@lucasribeiro.co',
+  //   },
+  // ]
+
+  if (isLoading) return (
+    <p>Loading...</p>
+  )
+
+  if (!passwords) return <p>Unable to fetch passwords.</p>
 
   return (
     <div className='Password__List'>
-      {passwords.map((password, index) =>
+      {passwords && passwords.map((password, index) =>
         <Password key={index} password={password} />
       )}
     </div>
