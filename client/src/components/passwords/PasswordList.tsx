9d68ec94bac9fa4ee9f915c28dbe7168bdb7a59e
LucasCodes01 <lucascodesweb@gmail.com>
2022-12-18 15:01:13 +1300

    Removed hard-coded password list

--- client/src/components/passwords/PasswordList.tsx
+++ client/src/components/passwords/PasswordList.tsx
@@ -30,44 +30,6 @@ const PasswordList = () => {
     fetchPasswords();
   }, [])
 
-  // const passwords = [
-  //   {
-  //     icon: 'Google.png',
-  //     site: 'Google',
-  //     email: 'lucas@lucasribeiro.co',
-  //   },
-  //   {
-  //     icon: 'Google.png',
-  //     site: 'Chess',
-  //     email: 'lucas@lucasribeiro.co',
-  //   },
-  //   {
-  //     icon: 'Google.png',
-  //     site: 'Netflix',
-  //     email: 'lucas@lucasribeiro.co',
-  //   },
-  //   {
-  //     icon: 'Google.png',
-  //     site: 'Spotify',
-  //     email: 'lucas@lucasribeiro.co',
-  //   },
-  //   {
-  //     icon: 'Google.png',
-  //     site: 'YouTube',
-  //     email: 'lucas@lucasribeiro.co',
-  //   },
-  //   {
-  //     icon: 'Google.png',
-  //     site: 'Instagram',
-  //     email: 'lucas@lucasribeiro.co',
-  //   },
-  //   {
-  //     icon: 'Google.png',
-  //     site: 'GitHub',
-  //     email: 'lucas@lucasribeiro.co',
-  //   },
-  // ]
-
   if (isLoading) return (
     <p>Loading...</p>
   )
