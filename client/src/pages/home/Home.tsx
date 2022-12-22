4b43550cc5f99e6d4d642ee5ad33b2ead4ea4103
LucasCodes01 <lucascodesweb@gmail.com>
2022-12-22 17:50:16 +1300

    Created landing page

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,9 +1,21 @@
+import { Link } from 'react-router-dom';
 import Navbar from '../../components/navbar/Navbar';
+import './Home.scss';
 
 const Home = () => {
   return (
-    <div>
+    <div className='Home'>
       <Navbar />
+      <div className='Home__Container'>
+        <div className='Home__Container__Content'>
+          <h1>A <span>secure vault</span> for saving your passwords on the go.</h1>
+          <h3>You can store your passwords safely inside of your vault and access them from any device with an internet connection.</h3>
+          <Link to='/signup'>Create an account</Link>
+        </div>
+        <div className={`${'Home__Container__Image'} ${'bounce'}`}>
+          <img src='/home.svg' />
+        </div>
+      </div>
     </div>
   )
 }
