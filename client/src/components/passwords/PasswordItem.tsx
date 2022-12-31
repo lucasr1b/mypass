9c69331318e956a4ea094681fa2b53967fd4f066
LucasCodes01 <lucascodesweb@gmail.com>
2022-12-31 13:19:43 -0300

    Added more actions button to password item

--- client/src/components/passwords/PasswordItem.tsx
+++ client/src/components/passwords/PasswordItem.tsx
@@ -1,6 +1,6 @@
 import { Password } from '../../utils/types';
 import './PasswordItem.scss';
-import { ChevronRight } from 'react-bootstrap-icons';
+import { ThreeDotsVertical } from 'react-bootstrap-icons';
 
 type PasswordProps = {
 	password: Password,
@@ -8,15 +8,24 @@ type PasswordProps = {
 }
 
 const PasswordItem = (props: PasswordProps) => {
+
+	const handleOptions = (e: any) => {
+		e.preventDefault();
+
+		console.log('Hello World');
+	}
+
 	return (
-		<div className='PasswordItem' onClick={props.onClick}>
-			<img src={props.password.logo} alt={props.password.logo} />
-			<div className='PasswordItem__Details'>
-				<span className='PasswordItem__Details__Identifier'>{props.password.identifier}</span>
-				<span className='PasswordItem__Details__Email'>{props.password.details}</span>
+		<div className='PasswordItem'>
+			<div className='PasswordItem__Info' onClick={props.onClick}>
+				<img src={props.password.logo} alt={props.password.logo} />
+				<div className='PasswordItem__Details'>
+					<span className='PasswordItem__Details__Identifier'>{props.password.identifier}</span>
+					<span className='PasswordItem__Details__Email'>{props.password.details}</span>
+				</div>
 			</div>
-			<div className='PasswordItem__View'>
-				<ChevronRight />
+			<div className='PasswordItem__Actions' onClick={handleOptions}>
+				<ThreeDotsVertical />
 			</div>
 		</div>
 	);
