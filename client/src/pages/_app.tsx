0bb47b764aa9163c13bc0b3833142956c5170081
lucasr1b <lucascodesweb@gmail.com>
2023-01-16 10:12:26 -0300

    Migrated GoogleOAuthProvider client id to Next.js env variable

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,16 +3,14 @@ import '../styles/global.scss'
 import '../styles/theme.scss'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
 
 export const App = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter()
   return (
     <>
       <Head>
         <title>mypass</title>
       </Head>
-      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID as string}>
+      <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string}>
         <Component {...pageProps} />
       </GoogleOAuthProvider>
     </>
