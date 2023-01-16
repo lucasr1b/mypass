d7e3e44dd7902dbce9b6ca5b063300044fd4d361
lucasr1b <lucascodesweb@gmail.com>
2023-01-15 21:21:40 -0300

    Added profile pictures using jdenticon and email addresses

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import Backdrop from '../common/Backdrop';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOut } from '@fortawesome/free-solid-svg-icons';
+import * as jdenticon from 'jdenticon';
 
 type NavbarProps = {
   isHome?: boolean;
@@ -18,6 +19,25 @@ const Navbar = (props: NavbarProps) => {
 
   const [dropdownToggled, setDropdownToggled] = useState(false);
 
+  const jdenticon_config = {
+    hues: [260],
+    lightness: {
+      color: [0.35, 0.44],
+      grayscale: [0.35, 0.45]
+    },
+    saturation: {
+      color: 1.00,
+      grayscale: 1.00
+    },
+    backColor: "#0000"
+  };
+
+  jdenticon.configure(jdenticon_config);
+
+  const svgString = jdenticon.toSvg(localStorage.getItem('email'), 64);
+  const svg = new Blob([svgString], { type: "image/svg+xml" });
+  const url = URL.createObjectURL(svg)
+
   return (
     <nav className={'Navbar'}>
       <Link to='/'><img src='/logo.svg' className='Navbar__Logo' alt={'mypass'} /></Link>
@@ -27,7 +47,10 @@ const Navbar = (props: NavbarProps) => {
             <button className='Navbar__Theme' onClick={props.switchTheme} aria-label='theme'>{props.theme === 'light' ? <MoonFill /> : <SunFill />}</button>
             <div className='Navbar__Profile'>
               <button onClick={() => setDropdownToggled(!dropdownToggled)}>
-                <img src='profile.jpg' alt={'profile'} /> <span>{localStorage.getItem('name')?.split(' ')[0]}</span> {dropdownToggled ? <CaretUpFill /> : <CaretDownFill />}
+                <div className='Navbar__Profile__Picture'>
+                  <img src={url} alt='profile' />
+                </div>
+                <span>{localStorage.getItem('name')?.split(' ')[0]}</span> {dropdownToggled ? <CaretUpFill /> : <CaretDownFill />}
               </button>
               {dropdownToggled &&
                 <>
@@ -42,7 +65,8 @@ const Navbar = (props: NavbarProps) => {
           </div>
         </>
       }
-      {props.isHome &&
+      {
+        props.isHome &&
         <div className='Navbar__Buttons'>
           {!props.isLoggedIn ?
             <>
@@ -55,7 +79,7 @@ const Navbar = (props: NavbarProps) => {
 
         </div>
       }
-    </nav>
+    </nav >
   )
 }
 
