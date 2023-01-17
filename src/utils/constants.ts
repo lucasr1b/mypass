64e524f8f18018ee02bbdab5cc0820c19d85abf9
lucasr1b <lucascodesweb@gmail.com>
2023-01-17 16:55:05 -0300

    Made API_URL NEXT_PUBLIC env variable

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 import { AxiosRequestConfig } from 'axios';
 
-export const API_URL = process.env.API_URL;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const axiosConfig: AxiosRequestConfig = {
   headers: {
