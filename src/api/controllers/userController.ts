d65e0d52a6da237d7366105123284c0951e75b46
lucasr1b <lucascodesweb@gmail.com>
2023-01-17 23:16:42 -0300

    Removed testing and debugging (fixed external error on prod)

--- src/api/controllers/userController.ts
+++ src/api/controllers/userController.ts
@@ -7,8 +7,11 @@ dbConnect();
 
 export const getUserProfileController = async (req: NextApiRequest, res: NextApiResponse) => {
   const user = await req.session.user;
-  // const userData = await User.find({ '_id': new ObjectId(user.id) });
-  const userData = await User.findOne({ '_id': new ObjectId('639d81217c8b8b5334c57cad') }); // Hard coded for debugging production
 
-  res.status(200).json(userData);
+  if (user) {
+    const userData = await User.find({ '_id': new ObjectId(user.id) });
+    res.status(200).json(userData);
+  } else {
+    res.status(401).json({ loggedIn: false });
+  }
 }
